f039a51f4041c01d4745003a673ea69f6011363a
hobrien <heath.obrien@gmail.com>
2014-05-28 17:06:32 +0100

    moved most of the plotting and most of the merging to functions so there will be less repition

--- DESeq_v_DGEClust.R
+++ DESeq_v_DGEClust.R
@@ -5,6 +5,56 @@ library(VennDiagram)
 
 setwd("/Users/HeathOBrien/")
 
+merge_results<-function(DEseq, DGEclust, name, textfile){
+  #rename columns and add info about species and comparison
+  DEseq["samples"]<-name
+  DEseq<-rename(DEseq, c("pval" = "DESeq_pval", "padj"="DESeq_padj"))
+
+  #sort rows
+  DGEClust<-DGEClust[order(as.numeric(rownames(DGEClust))),]
+
+  #add DGESClust data to results
+  DEseq["DGEClust_pval"]<-DGEClust["pval"]
+  DEseq["DGEClust_padj"]<-DGEClust["padj"]
+
+  #write list of overlaps to textfile
+  write.table(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ], textfile)
+
+  #remove missing values that will cause an error in the following step
+  DEseq<-na.omit(DEseq)
+
+  #this will replace extreme values with zero, which makes the plot nicer
+  DEseq[DEseq$DESeq_padj < 1e-5, ]["DESeq_padj"]<-0
+  return(DEseq)
+}
+scatterplot<-function(DEseq, name) {
+  #make scatterplot of adjusted p-values and Venn Diagram of overlap
+  sp =  ggplot(DEseq, aes(x=DESeq_padj, y=DGEClust_padj)) +
+    geom_point(size=1.2) +
+    scale_x_log10() +
+    scale_y_log10() +
+    geom_hline(yintercept=0.1, size=.2) +
+    geom_vline(xintercept=0.1, size=.2) +
+    theme_bw() +
+    ggtitle(name)
+    return(sp)
+}
+vennplot <-function(DEseq) {
+  draw.pairwise.venn(area1=nrow(DEseq[DEseq$DGEClust_padj < 0.1, ]), 
+    area2=nrow(DEseq[DEseq$DESeq_padj < 0.1, ]), 
+    cross.area=nrow(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ]), 
+    fill=c('blue', 'red'), 
+    cat.col = c('blue', 'red'),
+    category=c("DESeq", "DGEClust"), 
+    lty = 'blank',
+    cex = 2,
+    cat.cex = 1.75,
+    margin=0.2,
+    fontfamily='sans',
+    cat.fontfamily='sans')
+ }
+
+
 #read in raw count data
 selaginellaCountTable =  read.table( file="Bioinformatics/Selaginella/Counts/Selag_counts.txt" , header=TRUE, row.names=1 )
 
@@ -20,213 +70,21 @@ KRAUS_cds <- estimateSizeFactors(KRAUS_cds)
 KRAUS_cds <- estimateDispersions(KRAUS_cds, method="blind", sharingMode="fit-only")
 
 # =======================================================================================
-#test for differential expression of KRAUS1 and KRAUS2
-DEseq<- nbinomTest(KRAUS_cds, "leaf1", "leaf2")
+#I'm not sure how best to loop this
+KRAUS12 <- nbinomTest(KRAUS_cds, "leaf1", "leaf2")
+#KRAUS13 <- nbinomTest(KRAUS_cds, "leaf1", "leaf3")
+#KRAUS14 <- nbinomTest(KRAUS_cds, "leaf1", "leaf4")
+#KRAUS23 <- nbinomTest(KRAUS_cds, "leaf2", "leaf3")
+#KRAUS24 <- nbinomTest(KRAUS_cds, "leaf2", "leaf4")
+#KRAUS34 <- nbinomTest(KRAUS_cds, "leaf3", "leaf4")
 
-#rename columns and add info about species and comparison
-DEseq["samples"]<-"12"
-DEseq["species"]<-"KRAUS"
-DEseq<-rename(DEseq, c("pval" = "DESeq_pval", "padj"="DESeq_padj"))
-
-#read in results from DGEClust
 DGEClust<-read.table(file="Bioinformatics/Selaginella/DGEClust/KRAUS12_pvals.txt", header=T)
+textfile<-"Google Drive/Selaginella/DGEClust/KRAUS12_overlap.txt"
+plotfile<-"Google Drive/Selaginella/DGEClust/KRAUS12_overlap.pdf"  
+name = "KRAUS12"
 
-#sort rows
-DGEClust<-DGEClust[order(as.numeric(rownames(DGEClust))),]
-
-#add DGESClust data to results
-DEseq["DGEClust_pval"]<-DGEClust["pval"]
-DEseq["DGEClust_padj"]<-DGEClust["padj"]
-
-write.table(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ], 
-  "Google Drive/Selaginella/DGEClust/KRAUS12_overlap.txt")
-
-#this will replace extreme values with zero, which makes the plot nicer
-DEseq[DEseq$DESeq_padj < 1e-5, ]["DESeq_padj"]<-0
-
-#make scatterplot of adjusted p-values and Venn Diagram of overlap
-pdf(file="Google Drive/Selaginella/DGEClust/KRAUS12.pdf")
-ggplot(DEseq, aes(x=DESeq_padj, y=DGEClust_padj)) +
-  geom_point(size=1.2) +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_hline(yintercept=0.1, size=.2) +
-  geom_vline(xintercept=0.1, size=.2) +
-  theme_bw() +
-  ggtitle("KRAUS12")
-  
-draw.pairwise.venn(area1=nrow(DEseq[DEseq$DGEClust_padj < 0.1, ]), 
-  area2=nrow(DEseq[DEseq$DESeq_padj < 0.1, ]), 
-  cross.area=nrow(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ]), 
-  fill=c('blue', 'red'), 
-  cat.col = c('blue', 'red'),
-  category=c("DESeq", "DGEClust"), 
-  lty = 'blank',
-  cex = 2,
-  cat.cex = 1.75,
-  margin=0.2,
-  fontfamily='sans',
-  cat.fontfamily='sans')
-  
-dev.off()
-
-# =======================================================================================
-#test for differential expression of KRAUS1 and KRAUS3
-DEseq<- nbinomTest(KRAUS_cds, "leaf1", "leaf3")
-
-#rename columns and add info about species and comparison
-DEseq["samples"]<-"13"
-DEseq["species"]<-"KRAUS"
-DEseq<-rename(DEseq, c("pval" = "DESeq_pval", "padj"="DESeq_padj"))
-
-#read in results from DGEClust
-DGEClust<-read.table(file="Bioinformatics/Selaginella/DGEClust/KRAUS13_pvals.txt", header=T)
-
-#sort rows
-DGEClust<-DGEClust[order(as.numeric(rownames(DGEClust))),]
-
-#add DGESClust data to results
-DEseq["DGEClust_pval"]<-DGEClust["pval"]
-DEseq["DGEClust_padj"]<-DGEClust["padj"]
-
-write.table(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ], 
-  "Google Drive/Selaginella/DGEClust/KRAUS13_overlap.txt")
-
-#remove missing values that will cause an error in the following step
-DEseq<-na.omit(DEseq)
-
-#this will replace extreme values with zero, which makes the plot nicer
-DEseq[DEseq$DESeq_padj < 1e-5, ]["DESeq_padj"]<-0
-
-#make scatterplot of adjusted p-values and Venn Diagram of overlap
-pdf(file="Google Drive/Selaginella/DGEClust/KRAUS13.pdf")
-ggplot(DEseq, aes(x=DESeq_padj, y=DGEClust_padj)) +
-  geom_point(size=1.2) +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_hline(yintercept=0.1, size=.2) +
-  geom_vline(xintercept=0.1, size=.2) +
-  theme_bw() +
-  ggtitle("KRAUS13")
-  
-draw.pairwise.venn(area1=nrow(DEseq[DEseq$DGEClust_padj < 0.1, ]), 
-  area2=nrow(DEseq[DEseq$DESeq_padj < 0.1, ]), 
-  cross.area=nrow(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ]), 
-  fill=c('blue', 'red'), 
-  cat.col = c('blue', 'red'),
-  category=c("DESeq", "DGEClust"), 
-  lty = 'blank',
-  cex = 2,
-  cat.cex = 1.75,
-  margin=0.2,
-  fontfamily='sans',
-  cat.fontfamily='sans')
-  
-dev.off()
-
-# =======================================================================================
-#test for differential expression of KRAUS1 and KRAUS4
-DEseq<- nbinomTest(KRAUS_cds, "leaf1", "leaf4")
-
-#rename columns and add info about species and comparison
-DEseq["samples"]<-"14"
-DEseq["species"]<-"KRAUS"
-DEseq<-rename(DEseq, c("pval" = "DESeq_pval", "padj"="DESeq_padj"))
-
-#read in results from DGEClust
-DGEClust<-read.table(file="Bioinformatics/Selaginella/DGEClust/KRAUS14_pvals.txt", header=T)
-
-#sort rows
-DGEClust<-DGEClust[order(as.numeric(rownames(DGEClust))),]
-
-#add DGESClust data to results
-DEseq["DGEClust_pval"]<-DGEClust["pval"]
-DEseq["DGEClust_padj"]<-DGEClust["padj"]
-
-write.table(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ], 
-  "Google Drive/Selaginella/DGEClust/KRAUS14_overlap.txt")
-
-#remove missing values that will cause an error in the following step
-DEseq<-na.omit(DEseq)
-
-#this will replace extreme values with zero, which makes the plot nicer
-DEseq[DEseq$DESeq_padj < 1e-5, ]["DESeq_padj"]<-0
-
-#make scatterplot of adjusted p-values and Venn Diagram of overlap
-pdf(file="Google Drive/Selaginella/DGEClust/KRAUS14.pdf")
-ggplot(DEseq, aes(x=DESeq_padj, y=DGEClust_padj)) +
-  geom_point(size=1.2) +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_hline(yintercept=0.1, size=.2) +
-  geom_vline(xintercept=0.1, size=.2) +
-  theme_bw() +
-  ggtitle("KRAUS14")
-  
-draw.pairwise.venn(area1=nrow(DEseq[DEseq$DGEClust_padj < 0.1, ]), 
-  area2=nrow(DEseq[DEseq$DESeq_padj < 0.1, ]), 
-  cross.area=nrow(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ]), 
-  fill=c('blue', 'red'), 
-  cat.col = c('blue', 'red'),
-  category=c("DESeq", "DGEClust"), 
-  lty = 'blank',
-  cex = 2,
-  cat.cex = 1.75,
-  margin=0.2,
-  fontfamily='sans',
-  cat.fontfamily='sans')
-  
-dev.off()
-# =======================================================================================
-#test for differential expression of KRAUS2 and KRAUS3
-DEseq<- nbinomTest(KRAUS_cds, "leaf2", "leaf3")
-
-#rename columns and add info about species and comparison
-DEseq["samples"]<-"23"
-DEseq["species"]<-"KRAUS"
-DEseq<-rename(DEseq, c("pval" = "DESeq_pval", "padj"="DESeq_padj"))
-
-#read in results from DGEClust
-DGEClust<-read.table(file="Bioinformatics/Selaginella/DGEClust/KRAUS23_pvals.txt", header=T)
-
-#sort rows
-DGEClust<-DGEClust[order(as.numeric(rownames(DGEClust))),]
-
-#add DGESClust data to results
-DEseq["DGEClust_pval"]<-DGEClust["pval"]
-DEseq["DGEClust_padj"]<-DGEClust["padj"]
-
-write.table(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ], 
-  "Google Drive/Selaginella/DGEClust/KRAUS23_overlap.txt")
-
-#remove missing values that will cause an error in the following step
-DEseq<-na.omit(DEseq)
-
-#this will replace extreme values with zero, which makes the plot nicer
-DEseq[DEseq$DESeq_padj < 1e-5, ]["DESeq_padj"]<-0
-
-#make scatterplot of adjusted p-values and Venn Diagram of overlap
-pdf(file="Google Drive/Selaginella/DGEClust/KRAUS23.pdf")
-ggplot(DEseq, aes(x=DESeq_padj, y=DGEClust_padj)) +
-  geom_point(size=1.2) +
-  scale_x_log10() +
-  scale_y_log10() +
-  geom_hline(yintercept=0.1, size=.2) +
-  geom_vline(xintercept=0.1, size=.2) +
-  theme_bw() +
-  ggtitle("KRAUS23")
-  
-draw.pairwise.venn(area1=nrow(DEseq[DEseq$DGEClust_padj < 0.1, ]), 
-  area2=nrow(DEseq[DEseq$DESeq_padj < 0.1, ]), 
-  cross.area=nrow(DEseq[DEseq$DESeq_padj < 0.1 & DEseq$DGEClust_padj < 0.1, ]), 
-  fill=c('blue', 'red'), 
-  cat.col = c('blue', 'red'),
-  category=c("DESeq", "DGEClust"), 
-  lty = 'blank',
-  cex = 2,
-  cat.cex = 1.75,
-  margin=0.2,
-  fontfamily='sans',
-  cat.fontfamily='sans')
-  
+KRAUS12<-merge_results(KRAUS12, DGEClust, name, textfile)
+pdf(plotfile)
+scatterplot(KRAUS12, name)
+vennplot(KRAUS12)
 dev.off()
