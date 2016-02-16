1b4225f4d0dabcb3907a53bcb3abd0737b1920e6
hobrien <heath.obrien@gmail.com>
2016-02-16 14:10:44 +0000

    added a blank theme

--- FormatGGplot.R
+++ FormatGGplot.R
@@ -1,3 +1,7 @@
+library(RColorBrewer)
+library(scales)
+library(grid)
+
 fte_theme <- function() {
     
     # Generate the colors for the chart procedurally with RColorBrewer
@@ -38,6 +42,46 @@ fte_theme <- function() {
         # Plot margins
         theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
 }
+blank_theme <- function() {
+    
+    # Generate the colors for the chart procedurally with RColorBrewer
+    color.background = "white"
+    color.grid.major = "white"
+    color.axis.text = "black"
+    color.axis.title = "black"
+    color.title = "white"
+    
+    # Begin construction of chart
+    theme_bw(base_size=9) +
+        
+        # Set the entire chart region to a light gray color
+        theme(panel.background=element_rect(fill=color.background, color=color.background)) +
+        theme(plot.background=element_rect(fill=color.background, color=color.background)) +
+        theme(panel.border=element_rect(color="black")) +
+        
+        # Format the grid
+        theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
+        theme(panel.grid.minor=element_blank()) +
+        theme(axis.ticks=element_blank()) +
+        
+        # Format the legend, but hide by default
+        theme(legend.position="none") +
+        theme(legend.background = element_rect(fill=color.background)) +
+        theme(legend.key = element_rect(fill=color.background, colour=color.background)) +
+        theme(legend.text = element_text(size=9,color=color.axis.title)) +
+        theme(legend.title = element_blank()) +
+        
+        # Set title and axis labels, and format these and tick marks
+        theme(plot.title=element_text(color=color.title, size=10, vjust=1.25)) +
+        theme(axis.text.x=element_text(size=9,color=color.axis.text)) +
+        theme(axis.text.y=element_text(size=9,color=color.axis.text)) +
+        theme(axis.title.x=element_text(size=10,color=color.axis.title, vjust=0)) +
+        theme(axis.title.y=element_text(size=10,color=color.axis.title, vjust=1.25)) +
+        
+        # Plot margins
+        theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
+}
+
 reverselog_trans <- function(base = exp(1)) {
      trans <- function(x) -log(x, base)
      inv <- function(x) base^(-x)
